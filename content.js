d9e7c378f6d45dcee41b0833b68e0481d7933e97
Eduardo Lorenzo <ejlorenzo19@gmail.com>
2025-09-28 00:25:29 +0200

    Initial commit

---
+++ content.js
@@ -0,0 +1,31 @@
+// =============== FUNCTIONS ===============
+const removePromotedPostsFromReddit = () => {
+	document.querySelectorAll('shreddit-ad-post').forEach((el) => el.remove());
+};
+
+const removePostsLabeledAs = (labels = []) => {
+	labels.forEach((label) => {
+		Array.from(document.querySelectorAll('article'))
+			.filter((el) =>
+				el.querySelector('span[slot="credit-bar"]').textContent.includes(label)
+			)
+			.forEach((el) => el.remove());
+	});
+};
+
+// =============== MAIN ===============
+const main = () => {
+	removePromotedPostsFromReddit();
+	removePostsLabeledAs([
+		'Popular near you',
+		'Based on your recent activity',
+		'Suggested for you',
+		'Suggested',
+		// "Because you've shown interest in a similar community",
+		// "Because you've shown interest in a similar post"
+		"Because you've"
+	]);
+};
+
+// =============== MAIN LOOP ===============
+setInterval(main, 500);
