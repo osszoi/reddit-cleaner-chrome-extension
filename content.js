269f48e5bd75d0c314679e40575e83c2fd36a38f
Eduardo Lorenzo <ejlorenzo19@gmail.com>
2026-02-13 10:11:29 +0100

    remove posts with 'join' and count removed posts

--- content.js
+++ content.js
@@ -1,30 +1,59 @@
 // =============== FUNCTIONS ===============
 const removePromotedPostsFromReddit = () => {
-	document.querySelectorAll('shreddit-ad-post').forEach((el) => el.remove());
+  let removed = 0;
+
+  document.querySelectorAll("shreddit-ad-post").forEach((el) => {
+    removed++;
+    el.remove();
+  });
+
+  document
+    .querySelectorAll("shreddit-post shreddit-join-button")
+    .forEach((el) => {
+      removed++;
+      el.remove();
+    });
+
+  return removed;
 };
 
 const removePostsLabeledAs = (labels = []) => {
-	labels.forEach((label) => {
-		Array.from(document.querySelectorAll('article'))
-			.filter((el) =>
-				el.querySelector('span[slot="credit-bar"]').textContent.includes(label)
-			)
-			.forEach((el) => el.remove());
-	});
+  let removed = 0;
+
+  labels.forEach((label) => {
+    Array.from(document.querySelectorAll("article"))
+      .filter((el) =>
+        el.querySelector('span[slot="credit-bar"]').textContent.includes(label)
+      )
+      .forEach((el) => {
+        removed++;
+        el.remove();
+      });
+  });
+
+  return removed;
 };
 
 // =============== MAIN ===============
 const main = () => {
-	removePromotedPostsFromReddit();
-	removePostsLabeledAs([
-		'Popular',
-		'Based on your recent activity',
-		'Suggested for you',
-		'Suggested',
-		// "Because you've shown interest in a similar community",
-		// "Because you've shown interest in a similar post"
-		"Because you've"
-	]);
+  const removedPromotedPosts = removePromotedPostsFromReddit();
+  const removedPostsLabeledAs = removePostsLabeledAs([
+    "Popular",
+    "Based on your recent activity",
+    "Suggested for you",
+    "Suggested",
+    // "Because you've shown interest in a similar community",
+    // "Because you've shown interest in a similar post"
+    "Because you've",
+    "Top posts by",
+  ]);
+
+  localStorage.setItem(
+    "removedPromotedPosts",
+    parseInt(localStorage.getItem("removedPromotedPosts") || 0) +
+      removedPromotedPosts +
+      removedPostsLabeledAs
+  );
 };
 
 // =============== MAIN LOOP ===============
